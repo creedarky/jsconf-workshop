7a8b9c83d20c0a70db3b27bfc260dd23a6034dc7
Maximiliano Castro <max@axiomzen.co>
2017-11-02 01:21:19 -0300

    add webpack

---
+++ webpack.client.config.js
@@ -0,0 +1,99 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const configs = {
+  development: require('./webpack/development.js'),
+  staging: require('./webpack/staging.js'),
+  production: require('./webpack/production.js'),
+};
+
+const ENV = process.env.NODE_ENV;
+
+const commonConfig = {
+  name: 'client',
+  target: 'web',
+  entry: {
+    index: path.join(__dirname, 'src/index.jsx'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [{
+      enforce: 'pre',
+      test: /\.jsx?$/,
+      use: 'eslint-loader',
+      exclude: /node_modules/,
+    }, {
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: 'babel-loader',
+    }, {
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [{
+          loader: 'css-loader',
+          options: {
+            sourceMap: true,
+            url: false,
+          },
+        }, {
+          loader: 'postcss-loader',
+          options: {
+            sourceMap: true,
+          },
+        }, {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true,
+            outputStyle: 'expanded',
+            includePaths: [
+              path.resolve(__dirname, 'src/styles'),
+            ],
+          },
+        }],
+      }),
+    }],
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+  },
+  plugins: [
+    new CleanWebpackPlugin([path.join(__dirname, 'dist')]),
+    new webpack.LoaderOptionsPlugin({
+      test: /\.jsx?$/,
+      options: {
+        eslint: {
+          emitWarning: ENV === 'development',
+          emitError: ENV === 'staging' || ENV === 'production',
+        },
+      },
+    }),
+    new StyleLintPlugin({
+      configFile: path.join(__dirname, '.stylelintrc'),
+      files: '**/*.?(sa|sc|c)ss',
+      context: path.join(__dirname, 'src'),
+      emitErrors: ENV !== 'development',
+    }),
+  ],
+};
+
+const environmentConfig = (() => {
+  switch (ENV) {
+    case 'production':
+      return configs.production;
+    case 'staging':
+      return configs.staging;
+    case 'development':
+    default:
+      return configs.development;
+  }
+})();
+
+module.exports = merge(commonConfig, environmentConfig);
---
+++ webpack.server.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const ENV = process.env.NODE_ENV || 'development';
+
+const sConfig = {
+  name: 'server',
+  target: 'node',
+  entry: './server/server.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2',
+    filename: 'server.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [{
+      enforce: 'pre',
+      test: /\.jsx?$/,
+      use: 'eslint-loader',
+      exclude: /node_modules/,
+    }, {
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: 'babel-loader',
+    }, {
+      test: /\.scss$/,
+      use: [{
+        loader: 'css-loader/locals',
+      }, {
+        loader: 'postcss-loader',
+      }, {
+        loader: 'sass-loader',
+        options: {
+          outputStyle: 'expanded',
+          includePaths: [
+            path.resolve(__dirname, 'src/styles'),
+          ],
+        },
+      }],
+    }],
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(ENV),
+        SERVER_SIDE: JSON.stringify(true),
+      },
+    }),
+  ],
+};
+
+module.exports = sConfig;
