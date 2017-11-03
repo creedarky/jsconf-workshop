26d2d00c47091153fadceeac5c10af0fcb9477b5
Maximiliano Castro <max@axiomzen.co>
2017-11-03 04:17:00 -0500

    add stats to prod build

--- webpack/production.js
+++ webpack/production.js
@@ -1,30 +1,37 @@
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const StatsWebpackPlugin = require('stats-webpack-plugin');
+
+const port = process.env.PORT || 3000;
 
 module.exports = {
   output: {
-    filename: '[name].[chunkhash].js'
+    filename: '[name].[chunkhash].js',
   },
   plugins: [
+    new StatsWebpackPlugin(`${name}-stats.json`, {
+      chunkModules: true,
+    }),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
+        NODE_ENV: JSON.stringify('production'),
+        API_URL: JSON.stringify(`http://localhost:${port}/api`),
+      },
     }),
     new ExtractTextPlugin({
-      filename: '[name].[chunkhash].css'
+      filename: '[name].[chunkhash].css',
     }),
     new webpack.LoaderOptionsPlugin({
       minimize: true,
-      debug: false
+      debug: false,
     }),
     new webpack.optimize.UglifyJsPlugin({
       mangle: {
-        screw_ie8: true
+        screw_ie8: true,
       },
       compress: {
-        screw_ie8: true
-      }
-    })
-  ]
+        screw_ie8: true,
+      },
+    }),
+  ],
 };
