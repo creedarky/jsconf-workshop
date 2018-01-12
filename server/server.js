f0688118913b834ce458758d6b1b07e7ce420714
Maximiliano Castro <max@axiomzen.co>
2018-01-12 13:10:14 -0300

    await for html response

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@
 import renderApp from '../src/server.jsx';
 
 module.exports = function serverRenderer({ clientStats, serverStats }) {
-  return (req, res, next) => {
-    const html = renderApp({ req, context: {}, clientStats });
+  return async (req, res, next) => {
+    const html = await renderApp({ req, context: {}, clientStats });
     res.status(200).send(html);
   };
 };
